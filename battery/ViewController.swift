2a2d65bd18a770e78eff534e581e246b1cd115a5
鈴木海渡 <5me2ab@bma.biglobe.ne.jp>
2017-10-29 15:45:01 +0900

    Initial Commit

---
+++ battery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  battery
+//
+//  Created by 鈴木海渡 on 2017/10/29.
+//  Copyright © 2017年 鈴木海渡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
